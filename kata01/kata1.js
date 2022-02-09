204a42e7e3fc79984c071b0dfc649d96aec6f076
Kelly Alzate <kejoh07@gmail.com>
2022-02-09 02:04:44 +0100

    se agrego matrizRules, colaboradores Pierreq, gaby, etneciv y kelly

--- kata01/kata1.js
+++ kata01/kata1.js
@@ -35,7 +35,7 @@ function matrizPrincipal(filas, columnas){
                 matrizInterna.push(cell);
             }
             
-            cell = " 0 ";
+            cell = 0;
             matrizInternaVecinos.push(cell);
         }
         matriz.push(matrizInterna);
@@ -45,7 +45,6 @@ function matrizPrincipal(filas, columnas){
         matrizInternaVecinos.splice();
 
     }
-
     for(let y=0;y<filas;y++){
         console.log(matriz[y].join(""));
     }
@@ -53,35 +52,95 @@ function matrizPrincipal(filas, columnas){
     for(let y=0;y<filas;y++){
         console.log(matrizVecinos[y].join(""));
     }
-
+    
     recorrerMatriz(matriz,matrizVecinos);
-    return matriz, matrizVecinos;
+    
+    matrizRules(matriz,matrizVecinos);
+    return matriz;
 }
 
 function recorrerMatriz(matriz, matrizVecinos)
-{   
+{
     var contarVecinos = 0;
     for(let y=0;y<filas;y++)
     {
         for(let x=1;x<=columnas;x++)
         {
+            
             if(matriz[y][x] == ' * ')
             {
                 //contarVecinos = contarVecinos + 1;
-                contarVecinos = matrizVecinos[y][x]  + 1; 
+                console.log(matrizVecinos[0][0])
+                console.log(matrizVecinos[0][0+1])
+                console.log(matrizVecinos[0][1]);
+                
+                contarVecinos = matrizVecinos[y][x+1]; 
                 matrizVecinos[y][x-1] = contarVecinos;
-            }
+            } // Nos faltó agregar otro ciclo para recorrer en otras direcciones, .map + function()
         }
-        //console.log(matriz[y].join("-")); //a-a-a =aaa matriz[y][x]más de 
     }
     for(let y=0;y<filas;y++){
         console.log(matrizVecinos[y].join(""));
     }
 }
 
+function matrizRules(matriz, matrizVecinos)
+{
+    
+    var cell;
+    var matrizInternaFinal = [];
+    var matrizFinal = [];
+    
+    for(let y=0;y<columnas;y++)
+    {
+        for(let x=0;x<filas;x++)
+        {
+          console.log(matrizVecinos[y][x]);
+          console.log(matrizVecinos[y][x] == 1);
+            if((matriz[y][x]) == " * ")
+            {
+                if(matrizVecinos[y][x] <2 || matrizVecinos[y][x] >3)
+                {
+                    cell = " . ";
+                    matrizInternaFinal.push(cell);
+                } // Regla 1 y 2
+                else if(matrizVecinos[y][x] == 2 || matrizVecinos[y][x] == 3)
+                {
+                    cell = " * ";
+                    matrizInternaFinal.push(cell);
+                } // Regla 3
+                else{
+                    console.log("hay un error")   
+                    }
+            }
+            else if((matriz[y][x]) == " . ")
+            {
+                if(matrizVecinos[y][x] == 3)
+                {
+                    cell = " * ";
+                    matrizInternaFinal.push(cell);
+                    // Regla 4
+                }
+            }   
+            else{
+                console.log("entrando al else")
+            }
+
+        }
+
+        matrizFinal.push(matrizInternaFinal);
+        matrizInternaFinal.splice();
+    }
+    return matrizFinal;
+}
 
 
-//Cualquier célula viva con menos de 2 vecinos vivos muere  
-//Cualquier célula viva con más de 3 vecinos muere  ()
-//Cualquier célula viva con 2 o 3 vecinos vivos vive 
-//Cualquier célula con 3 vecinos vivos vive 
+/*Cualquier célula viva con menos de 2 vecinos vivos muere
+  Cualquier célula viva con más de 3 vecinos muere  ()
+  Cualquier célula viva con 2 o 3 vecinos vivos vive
+  Cualquier célula con 3 vecinos vivos vive
+
+1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+2. Any live cell with more than three live neighbours dies, as if by overcrowding.
+3. Any live cell with two or three live neighbours lives on to the next generation.
+4. Any dead cell with exactly three live neighbours becomes a live cell.*/
